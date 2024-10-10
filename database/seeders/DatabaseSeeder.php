d6b5e2f5202c963be339d3e4ba79d524682cef00
Mateus Camacho <mateus.camaccho@gmail.com>
2024-10-09 23:31:07 -0300

    criando livewire components

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,11 +18,15 @@ class DatabaseSeeder extends Seeder
         User::factory()->count(200)->create();
 
         User::query()->inRandomOrder()->limit(10)->get()
-            ->each(fn(User $u) =>
+            ->each(function (User $u) {
 
-                Project::factory()->create(['created_by' => $u->id])
+                $project = Project::factory()->create(['created_by' => $u->id]);
 
-            );
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+
+            });
+
+
 
     }
 
