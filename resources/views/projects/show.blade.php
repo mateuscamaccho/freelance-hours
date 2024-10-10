d45b76d8643c8e9f0a57141608886cc40a8daed7
Mateus Camacho <mateus.camaccho@gmail.com>
2024-10-10 01:01:20 -0300

    Iniciando componentes livewire

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,9 +1,12 @@
 <x-layouts.app>
 
+    <div class="grid grid-cols-3 gap-6">
+        <livewire:projects.show :$project />
+        <livewire:projects.proposals :$project />
 
-    <livewire:projects.show :$project />
+    </div>
 
 
-    <livewire:projects.proposals :$project />
+
 
 </x-layouts.app>
