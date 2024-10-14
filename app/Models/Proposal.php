45711f9c86a8070e3510bd6242d51458cc715168
Mateus Camacho <mateus.camaccho@gmail.com>
2024-10-14 10:09:44 -0300

    ajustes

--- app/Models/Proposal.php
+++ app/Models/Proposal.php
@@ -9,4 +9,7 @@ class Proposal extends Model
 {
     /** @use HasFactory<\Database\Factories\ProposalFactory> */
     use HasFactory;
+
+    protected $fillable = ['email', 'hours'];
+
 }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,7 +3,6 @@
     <div class="grid grid-cols-3 gap-6">
         <livewire:projects.show :$project />
         <livewire:projects.proposals :$project />
-
     </div>
 
 
